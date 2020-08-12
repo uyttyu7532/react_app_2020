5ae0ec304749dd9266b8f07f5dd08d85887deea6
choyerin <uyttyu7532@naver.com>
2020-08-13 06:23:34 +0900

    backup

--- src/App.js
+++ src/App.js
@@ -1,46 +1,26 @@
 import React from 'react';
-import PropTypes from "prop-types"
 
-function FREITAG({name, url, rating}){
-return (
-<div>
-<h3>{rating}</h3>
-<a href={url}>{name}</a>
-</div>);
-}
-
-FREITAG.propTypes = {
-  name:PropTypes.string.isRequired,
-  url:PropTypes.string.isRequired,
-  rating:PropTypes.number.isRequired
-}
-
-
-
-const bags = [
-  {
-    id:1,
-    name:"F41 HAWAII FIVE-O",
-    url:"https://www.freitag.ch/en/f41",
-    rating:5
-  },
-  {
-    id:2,
-    name:"F153 JAMIE",
-    url:"https://www.freitag.ch/en/f153",
-    rating:4
-  },
-  {
-    id:3,
-    name:"F11 LASSIE",
-    url:"https://www.freitag.ch/en/f11",
-    rating:3
+class App extends React.Component{
+  state = {
+    count: 0
+  };
+  add = () => {
+    console.log("add");
+    this.setState(current=>({count: current.count+1}));
   }
-]
-
-function App() {
-  return <div>
-    {bags.map(bag => <FREITAG key={bag.id} name={bag.name} url={bag.url} rating={bag.rating}/>)}
+  minus = () => {
+    console.log("minus");
+    this.setState(current=>({count: current.count-1}));
+  }
+  render(){
+  return (
+    <div>
+    <h1>Number is {this.state.count}</h1>
+    <button onClick={this.add}>add</button>
+    <button onClick={this.minus}>minus</button>
     </div>
+  );
+  }
 }
+
 export default App;
