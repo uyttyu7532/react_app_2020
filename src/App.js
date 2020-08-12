dfdb5100e541feca8933e1b0abefd65615bdc8fd
choyerin <uyttyu7532@naver.com>
2020-08-13 05:35:15 +0900

    새로운 예제

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
 import React from 'react';
+import PropTypes from "prop-types"
 
-function renderBags(bags){
-return <a href={bags.url}>{bags.name}</a>
+function FREITAG({name, url, rating}){
+return (
+<div>
+<h3>{rating}</h3>
+<a href={url}>{name}</a>
+</div>);
+}
+
+FREITAG.propTypes = {
+  name:PropTypes.string.isRequired,
+  url:PropTypes.string.isRequired,
+  rating:PropTypes.number.isRequired
 }
 
 
 
 const bags = [
   {
+    id:1,
     name:"F41 HAWAII FIVE-O",
-    url:"https://www.freitag.ch/en/f41"
+    url:"https://www.freitag.ch/en/f41",
+    rating:5
   },
   {
+    id:2,
     name:"F153 JAMIE",
-    url:"https://www.freitag.ch/en/f153"
+    url:"https://www.freitag.ch/en/f153",
+    rating:4
   },
   {
+    id:3,
     name:"F11 LASSIE",
-    url:"https://www.freitag.ch/en/f11"
+    url:"https://www.freitag.ch/en/f11",
+    rating:3
   }
 ]
 
 function App() {
   return <div>
-    {bags.map(renderBags)}
+    {bags.map(bag => <FREITAG key={bag.id} name={bag.name} url={bag.url} rating={bag.rating}/>)}
     </div>
 }
 export default App;
