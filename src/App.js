ff016d065e61df8030831b3b621a72f3908fd331
choyerin <uyttyu7532@naver.com>
2020-08-13 00:36:38 +0900

    Dynamic Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
 import React from 'react';
-import Potato from './potato'
+
+function FREITAG({name, url}){
+return <a href={url}>{name}</a>
+}
+
+
+
+const bags = [
+  {
+    name:"F41 HAWAII FIVE-O",
+    url:"https://www.freitag.ch/en/f41"
+  },
+  {
+    name:"F153 JAMIE",
+    url:"https://www.freitag.ch/en/f153"
+  },
+  {
+    name:"F11 LASSIE",
+    url:"https://www.freitag.ch/en/f11"
+  }
+]
 
 function App() {
   return <div>
-    <h1>app.js</h1>
-    <Potato/>
+    {bags.map(bag => <FREITAG name={bag.name} url={bag.url}/>)}
     </div>
 }
 export default App;
--- src/potato.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-function Potato(){
-    return <h3>hi~potato!</h3>;
-}
-
-export default Potato;
