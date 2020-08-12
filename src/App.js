11161d23167985b71be68c00b5716bfba0badaf4
choyerin <uyttyu7532@naver.com>
2020-08-12 23:18:32 +0900

    new component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Potato from './potato'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <div>
+    <h1>app.js</h1>
+    <Potato/>
     </div>
-  );
 }
-
 export default App;
---
+++ src/potato.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Potato(){
+    return <h3>hi~potato!</h3>;
+}
+
+export default Potato;
