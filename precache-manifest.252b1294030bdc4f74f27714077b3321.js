d1c11115a15e344717c13c414518891809fc90eb
choyerin <uyttyu7532@naver.com>
2020-08-14 02:01:12 +0900

    Updates

---
+++ precache-manifest.252b1294030bdc4f74f27714077b3321.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b70a3e2f10c6a7b751edc490f7c1827",
+    "url": "/react_app_2020/index.html"
+  },
+  {
+    "revision": "3f271bd2fe29593207ff",
+    "url": "/react_app_2020/static/js/2.2f08cab5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_app_2020/static/js/2.2f08cab5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "be92a79f69e2c7436307",
+    "url": "/react_app_2020/static/js/main.b9628b44.chunk.js"
+  },
+  {
+    "revision": "67a7f490dba7d162b872",
+    "url": "/react_app_2020/static/js/runtime-main.5d47c389.js"
+  }
+]);
