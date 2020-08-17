016bfd1092433f6f4c4ebded6147f96c8e803d83
choyerin <uyttyu7532@naver.com>
2020-08-18 03:27:50 +0900

    finish not css

--- src/App.js
+++ src/App.js
@@ -1,26 +1,56 @@
 import React from 'react';
+import axios from 'axios';
+import Movie from './Movie'
+import "./App.css";
 
-class App extends React.Component{
+class App extends React.Component {
   state = {
-    count: 0
+    isLoading: true,
+    movies: []
   };
-  add = () => {
-    console.log("add");
-    this.setState(current=>({count: current.count+1}));
+
+  getMovies = async () => {
+    // axios로부터 온 데이터를 잡는다. -> state에 이용
+    // 하지만 빠르지 않으므로 비동기식
+    const {
+      data: {
+        data: { movies }
+      }
+    } = await axios.get("https://yts.mx/api/v2/list_movies.json?sort_by=rating");
+    this.setState({ movies, isLoading: false })
+
+  };
+
+  componentDidMount() {
+    this.getMovies();
   }
-  minus = () => {
-    console.log("minus");
-    this.setState(current=>({count: current.count-1}));
-  }
-  render(){
-  return (
-    <div>
-    <h1>Number is {this.state.count}</h1>
-    <button onClick={this.add}>add</button>
-    <button onClick={this.minus}>minus</button>
-    </div>
-  );
-  }
-}
+
+  render() {
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+          {isLoading ? (
+          <div className="loader">
+            <span className="loader_text">Loading...</span>
+          </div>
+          ) : (
+            <div className="movies">
+            {movies.map(movie => (
+            <Movie
+              key = {movie.id}
+              id={movie.id}
+              year={movie.year} 
+              title={movie.title}
+              summary={movie.summary} 
+              poster={movie.medium_cover_image} 
+              genres={movie.genres}
+            />
+          ))}
+            </div>
+          )}
+      </section>
+    );
+            }
+          }
 
 export default App;
